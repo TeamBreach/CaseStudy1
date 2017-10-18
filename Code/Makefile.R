3489657cdb067ab6595c3d106ac2ddffaaa97f82
Arturo Casillas <nerano7g@gmail.com>
2017-10-18 15:24:34 -0500

    Better Makefile

--- Code/Makefile.R
+++ Code/Makefile.R
@@ -5,12 +5,20 @@
 #  Updated: October 18, 2017
 ######################################
 
+#Set working directory
+setwd("C:/Users/acasi/Documents")
+
 #Raw Data
-* Beers.csv - Raw dataset 1 of 2 used for the primary analysis questions of this project.
-* Breweries.csv - Raw dataset 2 of 2 used for the primary analysis questions of this project.
+* Beers.csv # Raw dataset 1 of 2 used for the primary analysis questions of this project.
+* Breweries.csv # Raw dataset 2 of 2 used for the primary analysis questions of this project.
 
 #Code for analysis and data cleanup
-* CaseStudy1CombinedCode1.R - Code used to clean, merge, and analyze the data as well as prepare plots
+## Gather and cleanup raw data files
+## Merge our two data
+## Produce our analyis including maps
+* source("CaseStudy1CombinedCode1.R") - Code used to clean, merge, and analyze the data as well as prepare plots
 
 #Presentation
-* CaseStudy1BootstrapDoc.Rmd - Markdown file that is used to generate the presentation.
+## This is an R markdown file that is selfcontained
+## Changes to the code will not automatically pass to the markdown file
+* source("CaseStudy1BootstrapDoc.Rmd") - Markdown file that is used to generate the presentation.
